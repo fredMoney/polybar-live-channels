4e70c907a96a8c24ef329d02203e744f97a9f66e
fredMoney <freddy.ngoulou@gmail.com>
2024-03-13 13:54:32 +0200

    Channels are sorted
    Channels are now sorted by the order they are in the input file

--- main.py
+++ main.py
@@ -3,8 +3,9 @@
 # call apis and retrieve live channels
 #   twitch DONEEEEEE
 #   youtube
-# merge live channels into 1 list and order based on file orderings
-# return first 3 channels
+# merge live channels into 1 list 
+# (DONE)and order based on file orderings
+# (DONE)return first 3 channels
 # clean this shit up
 import twitch
 
@@ -23,18 +24,29 @@ def split_channels(channels):
     return twitch_channels, youtube_channels
 
 
+def import_channels(location='channel_list.txt'):
+    with open(location, mode='r') as infile:
+        channels = infile.read().splitlines()
+    SORT_ORDER = {v[1:]:i for i,v in enumerate(channels)}
+    return channels, SORT_ORDER
+
+
 def format_channels(channels):
     message = ''.join(str(channel)+ '󱜠 ' for channel in channels[:3])
     return message
 
-# MAIN
-with open('channel_list.txt', mode='r') as infile:
-    channels = infile.read().splitlines()
-ttv_c, yt_c = split_channels(channels)
 
+def order_channels(channels, SORT_ORDER):
+    ordered = sorted(channels, key=lambda x: SORT_ORDER[x])
+    return ordered
+
+
+# MAIN
+channels, SORT_ORDER = import_channels()
+ttv_c, yt_c = split_channels(channels)
 access_token, expires_in = twitch.get_twitch_access_token()
 with open('tokens.txt', mode='w') as ofile:
     ofile.writelines([access_token, '\n', str(expires_in)])
 live_channels = twitch.get_twitch_streams(access_token, '74wu0uvvktrybwf259qa49r624hzwg', ttv_c)
-
-print(format_channels(live_channels))
+sorted_channels = order_channels(live_channels, SORT_ORDER)
+print(format_channels(sorted_channels))
