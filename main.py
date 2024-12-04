35a97618b70105921ad27882ba589b69ea232307
fredMoney <freddy.ngoulou@gmail.com>
2024-12-04 11:32:24 +0200

    The OOP update

--- main.py
+++ main.py
@@ -1,65 +1,19 @@
-import twitch
-import math
-import regex
+from twitch import TwitchQueryNameInterval
 
 CWD = '/home/fredd/Documents/polybar-live-channels/'
 
-def split_channels(channels):
-    twitch_channels = []
-    youtube_channels = []
-
-    for channel in channels:
-        if channel[0] == 't':
-            twitch_channels.append(channel[1:])
-        elif channel[0] == 'y':
-            youtube_channels.append(channel[1:])
-        else:
-            print('Invalid formatting: '.join(str(channel)))
-
-    return twitch_channels, youtube_channels
-
-
-def import_channels(location=CWD+'channel_list.txt'):
-    with open(location, mode='r') as infile:
-        channels = infile.read().splitlines()
-    SORT_ORDER = {v[1:]:i for i,v in enumerate(channels)}
-    return channels, SORT_ORDER
-
-
 def format_channels(channels):
-    message = ''.join(str(channel)+ '󱜠 ' for channel in channels[:3])
+    message = ''.join('󱜠 ' + str(channel)+ ' ' for channel in channels[:3])
     return message
-
 
 def order_channels(channels, SORT_ORDER):
     ordered = sorted(channels, key=lambda x: SORT_ORDER[x])
     return ordered
 
-
 # MAIN
-POLYBAR_INTERVAL = 600
-access_token = None
-uses = 0
-channels, SORT_ORDER = import_channels()
-ttv_c, yt_c = split_channels(channels)
-
-with open(CWD+'tokens.txt', mode='r') as token_file:
-    access_token = token_file.read()
-
-with open(CWD+'remaining_uses.txt', mode='r') as uses_file:
-    uses = int(uses_file.read())
-
-if uses < 1 or access_token is None:
-    access_token, expires_in = twitch.get_twitch_access_token()
-    uses = int(math.floor(expires_in / POLYBAR_INTERVAL))
-
-with open(CWD+'tokens.txt', mode='w') as ofile:
-    ofile.writelines(access_token)
-live_channels = twitch.get_twitch_streams(access_token, ttv_c)
-uses = uses - 1
-
-with open(CWD+'remaining_uses.txt', mode='w') as uses_file:
-    uses_file.write(str(uses))
-
-sorted_channels = order_channels(live_channels, SORT_ORDER)
-print(format_channels(sorted_channels))
+twitch_handler = TwitchQueryNameInterval()
+twitch_handler.get_credentials()
+twitch_handler.get_twitch_access_token()
+twitch_handler.import_channels(CWD + 'channel_list.txt')
+twitch_handler.get_info()
+print(format_channels(twitch_handler.info))
--- twitch.py
+++ twitch.py
@@ -1,46 +1,59 @@
 import requests
 import ast
+from abc import abstractmethod
 
 CWD = '/home/fredd/Documents/polybar-live-channels/'
 
-def get_credentials():
-    with open(CWD+'twitch_credentials.json', mode='r') as infile:
-        credentials = ast.literal_eval(infile.read())
-    return credentials
+class TwitchQuery():
+    def __init__(self):
+        self.credentials = {}
+        self.access_token = ""
+        self.expires_in = 0
+        self.channels_to_query = []
+        self.sort_order = {}
+        self.info = []
 
-def get_twitch_access_token():
-    credentials = get_credentials()
-    request_url = 'https://id.twitch.tv/oauth2/token'
-    request_json = {
-                        "client_id": str(credentials['client_id']),
-                        "client_secret": str(credentials['client_secret']),
-                        "grant_type": "client_credentials",
-                    }
-    response = requests.post(request_url, json=request_json)
-    access_token = response.json()['access_token']
-    expires_in = int(response.json()['expires_in'])
+    def get_credentials(self):
+        with open(CWD+'twitch_credentials.json', mode='r') as infile:
+            self.credentials = ast.literal_eval(infile.read())
 
-    return access_token, expires_in
+    def get_twitch_access_token(self):
+        request_url = 'https://id.twitch.tv/oauth2/token'
+        request_json = {
+                            "client_id": str(self.credentials['client_id']),
+                            "client_secret": str(self.credentials['client_secret']),
+                            "grant_type": "client_credentials",
+                        }
+        response = requests.post(request_url, json=request_json)
+        self.access_token = response.json()['access_token']
+        self.expires_in = int(response.json()['expires_in'])
 
+    def import_channels(self, location):
+        with open(location, mode='r') as infile:
+            self.channels_to_query = [channel.rstrip() for channel in infile]
+        self.sort_order = {v:i for i,v in enumerate(self.channels_to_query)}
 
-def get_twitch_streams(access_token, channels_to_query):
-    credentials = get_credentials()
-    client_id = str(credentials['client_id'])
-    live_channels = []
-    REQUEST_URL = 'https://api.twitch.tv/helix/streams'
-    headers = {
-                "Authorization": "Bearer {0}".format(access_token),
-                "Client-Id": client_id,
-              }
-    payload = {
-                "user_login": [str(channel) for channel in channels_to_query],
-                "type": "live",
-              }
-    response = requests.get(REQUEST_URL, headers=headers, params=payload)
-    for r in response.json()['data']:
-        live_channels.append(r['user_name'])
+    @abstractmethod
+    def get_info(self):
+        pass
 
-    return live_channels
-    
+class TwitchQueryNameInterval(TwitchQuery):
+    def __init__(self):
+        super().__init__()
 
+    def get_info(self):
+        client_id = str(self.credentials['client_id'])
+        REQUEST_URL = 'https://api.twitch.tv/helix/streams'
+        headers = {
+                    "Authorization": "Bearer {0}".format(self.access_token),
+                    "Client-Id": client_id,
+                  }
+        payload = {
+                    "user_login": [str(channel) for channel in self.channels_to_query],
+                    "type": "live",
+                  }
+        response = requests.get(REQUEST_URL, headers=headers, params=payload)
+        for r in response.json()['data']:
+            self.info.append(r['user_name'])
 
+        self.info = sorted(self.info, key=lambda channel: self.sort_order[channel])
